c7a3f436ef7628c8de176efa46bad6dd8318f8c1
Edgar Lopez <elopezanaya@gmail.com>
2023-12-26 20:55:01 -0800

    adding tests

--- cit-gh-create-pull-request/src/builders/builder.spec.test.ts
+++ cit-gh-create-pull-request/src/builders/builder.spec.test.ts
@@ -31,4 +31,36 @@ describe("buildPRPayload : happy path", () => {
       token: "token",
     });
   });
+
+
+  it("should return a GithubPayload object 2", async () => {
+    const core = {
+      getInput: jest.fn()
+      .mockReturnValueOnce("title")
+      .mockReturnValueOnce("body")
+      .mockReturnValueOnce("head")
+      .mockReturnValueOnce("base")
+      .mockReturnValueOnce("") // labels
+      .mockReturnValueOnce("") // assignees
+      .mockReturnValueOnce("") // reviewers
+      .mockReturnValueOnce("los-mas-aca") // team_reviewers
+      .mockReturnValueOnce("owner")
+      .mockReturnValueOnce("repo")
+      .mockReturnValueOnce("token"),
+    };
+    const result = await build(core as any);
+    expect(result).toEqual({
+      title: "title",
+      body: "body",
+      head: "head",
+      base: "base",
+      labels: [],
+      assignees: [],
+      reviewers: [],
+      team_reviewers: ["los-mas-aca"],
+      owner: "owner",
+      repo: "repo",
+      token: "token",
+    });
+  });
 });
